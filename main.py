fd164cca43db4c2dfc23a47f090ef6e875b56da0
Alexandre Camargo <141276980+AlexandreCamarg@users.noreply.github.com>
2025-09-22 21:45:47 -0300

    Test
    update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""Módulo principal da API com FastAPI para gerenciar estudantes."""
 
 import random
 from fastapi import FastAPI
@@ -7,7 +6,6 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class Estudante(BaseModel):
-    """Modelo que representa um estudante."""
     name: str
     curso: str
     ativo: bool
@@ -15,29 +13,24 @@ class Estudante(BaseModel):
 
 @app.get("/")
 def root():
-    """Rota raiz da API."""
     return {"message": "API rodando com sucesso 🚀"}
 
 
 @app.get("/teste")
 def funcaoteste():
-    """Rota de teste que retorna um número aleatório."""
     return {"teste": True, "num_aleatorio": random.randint(0, 100000)}
 
 
 @app.post("/estudante")
 def create_estudante(estudante: Estudante):
-    """Cria um novo estudante."""
     return estudante
 
 
 @app.put("/estudante/{id_estudante}")
 def update_estudante(id_estudante: int):
-    """Atualiza os dados de um estudante com base no ID."""
     return {"updated": id_estudante > 0}
 
 
 @app.delete("/estudante/{id_estudante}")
 def delete_estudante(id_estudante: int):
-    """Deleta um estudante com base no ID."""
     return {"deleted": id_estudante > 0}
