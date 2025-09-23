01755ca54527ba646343bf14ac7a77c37b90c6f0
Alexandre Camargo <141276980+AlexandreCamarg@users.noreply.github.com>
2025-09-22 21:43:21 -0300

    await

--- test/test.py
+++ test/test.py
@@ -1,50 +1,47 @@
-"""Testes unitários para o módulo main da API."""
-
+from src.main import *
 from unittest.mock import patch
 
-def test_root():
-    result = root()
-    yield result
+import pytest
+import pytest_asyncio
+
+
+@pytest.mark.asyncio
+async def test_root():
+    result = await root()
     assert result == {"message": "API rodando com sucesso 🚀"}
 
-
-def test_funcaoteste():
+@pytest.mark.asyncio
+async def test_funcaoteste():
     """Testa se a função de teste retorna o número aleatório esperado."""
     with patch("random.randint", return_value=12345):
-        result = funcaoteste()
-        yield result
+        result = await funcaoteste()
     assert result == {"teste": True, "num_aleatorio": 12345}
 
-
-def test_create_estudante():
+@pytest.mark.asyncio
+async def test_create_estudante():
     """Testa a criação de um estudante."""
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
-    result = create_estudante(estudante_teste)
-    yield result
+    result = await create_estudante(estudante_teste)
     assert estudante_teste == result
 
-
-def test_update_estudante_negativo():
-    result = update_estudante(-5) == {"updated": False}
-    yield result
+@pytest.mark.asyncio
+async def test_update_estudante_negativo():
+    result = await update_estudante(-5) == {"updated": False}
     assert not result
 
-
-def test_update_estudante_positivo():
-    result = update_estudante(10) == {"updated": True}
-    yield result
+@pytest.mark.asyncio
+async def test_update_estudante_positivo():
+    result = await update_estudante(10) == {"updated": True}
     assert result
 
-
-def test_delete_estudante_negativo():
-    result = delete_estudante(-5) == {"deleted": False}
-    yield result
+@pytest.mark.asyncio
+async def test_delete_estudante_negativo():
+    result = await delete_estudante(-5) == {"deleted": False}
     assert result
 
-
-def test_delete_estudante_positivo():
-    result = delete_estudante(10) == {"deleted": True}
-    yield result
+@pytest.mark.asyncio
+async def test_delete_estudante_positivo():
+    result = await delete_estudante(10) == {"deleted": True}
     assert result
 
 
