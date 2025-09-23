5ed15065d3f339871cf87261d74309245011eb32
Alexandre Camargo <141276980+AlexandreCamarg@users.noreply.github.com>
2025-09-22 22:00:03 -0300

    Ajuste
    Revisão

--- test/test.py
+++ test/test.py
@@ -1,46 +1,34 @@
 from src.main import *
 from unittest.mock import patch
 
-import pytest
-import pytest_asyncio
-
-
-@pytest.mark.asyncio
-async def test_root():
-    result = await root()
+def test_root():
+    result = root()
     assert result == {"message": "API rodando com sucesso 🚀"}
 
-@pytest.mark.asyncio
-async def test_funcaoteste():
+def test_funcaoteste():
     """Testa se a função de teste retorna o número aleatório esperado."""
     with patch("random.randint", return_value=12345):
-        result = await funcaoteste()
+        result = funcaoteste()
     assert result == {"teste": True, "num_aleatorio": 12345}
 
-@pytest.mark.asyncio
-async def test_create_estudante():
+def test_create_estudante():
     """Testa a criação de um estudante."""
     estudante_teste = Estudante(name="Fulano", curso="Curso 1", ativo=False)
-    result = await create_estudante(estudante_teste)
+    result = create_estudante(estudante_teste)
     assert estudante_teste == result
 
-@pytest.mark.asyncio
-async def test_update_estudante_negativo():
-    result = await update_estudante(-5) == {"updated": False}
-    assert not result
+def test_update_estudante_negativo():
+    result = update_estudante(-5)
+    assert result == {"updated": False}
 
-@pytest.mark.asyncio
-async def test_update_estudante_positivo():
-    result = await update_estudante(10) == {"updated": True}
-    assert result
+def test_update_estudante_positivo():
+    result = update_estudante(10)
+    assert result == {"updated": True}
 
-@pytest.mark.asyncio
-async def test_delete_estudante_negativo():
-    result = await delete_estudante(-5) == {"deleted": False}
-    assert result
+def test_delete_estudante_negativo():
+    result = delete_estudante(-5)
+    assert result == {"deleted": False}
 
-@pytest.mark.asyncio
-async def test_delete_estudante_positivo():
-    result = await delete_estudante(10) == {"deleted": True}
-    assert result
-    
+def test_delete_estudante_positivo():
+    result = delete_estudante(10)
+    assert result == {"deleted": True}
